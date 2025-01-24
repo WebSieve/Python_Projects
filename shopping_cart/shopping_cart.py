a77e1d615b3228765d95cfe84cf0f4109560cfb8
rimanshupatel <rimanshupatel1@gmail.com>
2025-01-25 00:56:04 +0530

    fixing issue

--- shopping_cart/shopping_cart.py
+++ shopping_cart/shopping_cart.py
@@ -1,23 +1,34 @@
 order = {}
-
 def shopping_cart():
-    while True:
-        # print current order
-        if order:
-           print("\nYour current orders : ")
-           for items, price in order.items():
-                print(f"{items.capitalize()} - ${price}\n")
+   global order
 
-        food = input("Enter the food item you want (or 'done' to finish) : ").lower()
-        # check point
-        if food == "done":
+   while True:
+         # print current order
+      if order:
+         print("\nYour current cart items : ")
+         for items, price in order.items():
+               print(f"{items.capitalize()} - ${price}")
+
+      food = input("\nEnter the food item you want (or 'done' to finish) : ").lower()
+      # check point
+      if food == "done":
          print("Thanks for Shopping !!")
          break
+      try:
+         price = float(input(f"Enter the price of {food.capitalize()} : $"))
+         if price < 0:
+            print("price can't be negative")
+         order[food] = price
+         
+      except ValueError:
+         print("Invalid price. Please enter a valid number.")
 
-        try:
-           price = float(input(f"Enter the price of {food.capitalize()} : $"))
-           order[food] = price
-        except ValueError:
-           print("Invalid price. Please enter a valid number.")
-
+         print(order)
+   if order:
+            total_price = sum(order.values())
+            print("\nYour final order:")
+            for items, price in order.items():
+                  print(f"{items.capitalize()} - ${price:.2f}")
+            print(f"Total Price: ${total_price:.2f}")
+         
 shopping_cart()
